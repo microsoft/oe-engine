3b78459a42f00bf2b8c44a108f7bf6bc70e0aac5
Dmitry Shmulevich <dmitsh@microsoft.com>
2018-10-08 15:41:07 -0700

    typo

--- test/acc-deploy-test.sh
+++ test/acc-deploy-test.sh
@@ -3,12 +3,12 @@
 if [[ -z "${SUBSCRIPTION_ID:-}" ]]; then echo "Must specify SUBSCRIPTION_ID"; exit 1; fi
 if [[ -z "${TENANT:-}" ]]; then echo "Must specify TENANT"; exit 1; fi
 
-if [[ -z "${SP_ID:-}" ]]; then echo "Must specify SP_ID"; exit 1; fi
-if [[ -z "${SP_PWD:-}" ]]; then echo "Must specify SP_PWD"; exit 1; fi
+if [[ -z "${SERVICE_PRINCIPAL_ID:-}" ]]; then echo "Must specify SERVICE_PRINCIPAL_ID"; exit 1; fi
+if [[ -z "${SERVICE_PRINCIPAL_PASSWORD:-}" ]]; then echo "Must specify SERVICE_PRINCIPAL_PASSWORD"; exit 1; fi
 
 if [[ -z "${LOCATION:-}" ]]; then echo "Must specify LOCATION"; exit 1; fi
 
-az login --service-principal -u ${SP_ID} -p ${SP_PWD} --tenant $TENANT
+az login --service-principal -u ${SERVICE_PRINCIPAL_ID} -p ${SERVICE_PRINCIPAL_PASSWORD} --tenant $TENANT
 az account set --subscription ${SUBSCRIPTION_ID}
 
 SSH_PUB_KEY=$(az keyvault secret show --vault-name oe-ci-test-kv --name id-rsa-oe-test-pub | jq -r .value | base64 -d)
