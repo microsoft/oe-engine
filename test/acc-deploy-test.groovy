70ddcb4bc7fdda9427370b01d50cea428459a96c
Dmitry Shmulevich <dmitsh@microsoft.com>
2018-10-08 22:28:58 -0700

    use credentials

--- test/acc-deploy-test.groovy
+++ test/acc-deploy-test.groovy
@@ -14,7 +14,9 @@ pipeline {
           sh 'rm -rf work'
           sh 'mkdir work'
           sh 'cp test/oe-lnx.json test/oe-win.json work/'
-          sh 'cd work; AZURE_CONFIG_DIR=. ../test/acc-deploy-test.sh'
+          withCredentials([usernamePassword(credentialsId: '40060061-6050-40f7-ac6a-53aeb767245f', passwordVariable: 'SERVICE_PRINCIPAL_PASSWORD', usernameVariable: 'SERVICE_PRINCIPAL_ID')]) {
+            sh 'cd work; AZURE_CONFIG_DIR=. ../test/acc-deploy-test.sh'
+          }
         }
       }
     }
