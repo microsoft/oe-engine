a78f7539c51f27455436b39b1a7f55a0126f11c1
Dmitry Shmulevich <dmitsh@microsoft.com>
2018-10-23 10:10:28 -0700

    cleanup

--- pkg/engine/template_generator.go
+++ pkg/engine/template_generator.go
@@ -137,8 +137,6 @@ func (t *TemplateGenerator) getTemplateFuncMap(cs *api.OpenEnclave) template.Fun
 			if err != nil {
 				// this should never happen and this is a bug
 				panic(fmt.Sprintf("BUG: %s", err.Error()))
-			} else {
-				//fmt.Printf(" get windows custom data from asset %s\n", b);
 			}
 			csStr := string(b)
 			csStr = strings.Replace(csStr, "SSH_PUB_KEY", cs.Properties.WindowsProfile.SSHPubKey, -1)
