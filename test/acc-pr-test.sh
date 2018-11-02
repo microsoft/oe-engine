d9eb78f864fc012d6cd7e46d8e73db8f92ae5eb8
Dmitry Shmulevich <dmitsh@microsoft.com>
2018-11-01 17:29:55 -0700

    debug

--- test/acc-pr-test.sh
+++ test/acc-pr-test.sh
@@ -20,8 +20,7 @@ if [[ -z "${SERVICE_PRINCIPAL_PASSWORD:-}" ]]; then echo "Must specify SERVICE_P
 LOCATION=eastus
 
 az login --service-principal -u ${SERVICE_PRINCIPAL_ID} -p ${SERVICE_PRINCIPAL_PASSWORD} --tenant ${TENANT_ID}
-#az account set --subscription ${SUBSCRIPTION_ID}
-az account set --subscription e5839dfd-61f0-4b2f-b06f-de7fc47b5998
+az account set --subscription ${SUBSCRIPTION_ID}
 
 SSH_PUB_KEY=$(az keyvault secret show --vault-name oe-ci-test-kv --name id-rsa-oe-test-pub | jq -r .value | base64 -d)
 sed -i "/\"keyData\":/c \"keyData\": \"${SSH_PUB_KEY}\"" oe-lnx.json
