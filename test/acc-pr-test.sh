514c6a4925d8c2c2e96296f3d7be3fc16b9425c5
Dmitry Shmulevich <dmitsh@microsoft.com>
2018-11-02 13:09:27 -0700

    debug

--- test/acc-pr-test.sh
+++ test/acc-pr-test.sh
@@ -11,6 +11,8 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 cd $DIR
 
+###
+
 if [[ -z "${SUBSCRIPTION_ID:-}" ]]; then echo "Must specify SUBSCRIPTION_ID"; exit 1; fi
 if [[ -z "${TENANT_ID:-}" ]]; then echo "Must specify TENANT_ID"; exit 1; fi
 
@@ -19,13 +21,10 @@ if [[ -z "${SERVICE_PRINCIPAL_PASSWORD:-}" ]]; then echo "Must specify SERVICE_P
 
 LOCATION=eastus
 
-echo "az login ..."
 az login --service-principal -u ${SERVICE_PRINCIPAL_ID} -p ${SERVICE_PRINCIPAL_PASSWORD} --tenant ${TENANT_ID}
-echo "az account set --subscription ${SUBSCRIPTION_ID}"
 az account set --subscription ${SUBSCRIPTION_ID}
-echo "az login OK"
+
 SSH_PUB_KEY=$(az keyvault secret show --vault-name oe-ci-test-kv --name id-rsa-oe-test-pub | jq -r .value | base64 -d)
-echo "SSH_PUB_KEY $SSH_PUB_KEY"
 sed -i "/\"keyData\":/c \"keyData\": \"${SSH_PUB_KEY}\"" oe-lnx.json
 ../bin/oe-engine generate --api-model oe-lnx.json
 
