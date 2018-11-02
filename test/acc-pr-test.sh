1799a02df89eaa1642e8120e58d59bd54e7d72b0
Dmitry Shmulevich <dmitsh@microsoft.com>
2018-11-01 23:39:12 -0700

    debug

--- test/acc-pr-test.sh
+++ test/acc-pr-test.sh
@@ -22,7 +22,7 @@ LOCATION=eastus
 echo "az login ..."
 az login --service-principal -u ${SERVICE_PRINCIPAL_ID} -p ${SERVICE_PRINCIPAL_PASSWORD} --tenant ${TENANT_ID}
 echo "az account set --subscription ${SUBSCRIPTION_ID}"
-az account set --subscription ${SUBSCRIPTION_ID}
+az account set --subscription c4fdda6e-bfbd-4b8e-9703-037b3a45bf37
 
 SSH_PUB_KEY=$(az keyvault secret show --vault-name oe-ci-test-kv --name id-rsa-oe-test-pub | jq -r .value | base64 -d)
 sed -i "/\"keyData\":/c \"keyData\": \"${SSH_PUB_KEY}\"" oe-lnx.json
