14628dac0cd30bc2fced3515d9ad9df5298c8b40
Dmitry Shmulevich <dmitsh@microsoft.com>
2018-11-02 12:51:41 -0700

    debug

--- test/acc-pr-test.sh
+++ test/acc-pr-test.sh
@@ -22,9 +22,10 @@ LOCATION=eastus
 echo "az login ..."
 az login --service-principal -u ${SERVICE_PRINCIPAL_ID} -p ${SERVICE_PRINCIPAL_PASSWORD} --tenant ${TENANT_ID}
 echo "az account set --subscription ${SUBSCRIPTION_ID}"
-az account set --subscription c4fdda6e-bfbd-4b8e-9703-037b3a45bf37
-
+az account set --subscription ${SUBSCRIPTION_ID}
+echo "az login OK"
 SSH_PUB_KEY=$(az keyvault secret show --vault-name oe-ci-test-kv --name id-rsa-oe-test-pub | jq -r .value | base64 -d)
+echo "SSH_PUB_KEY $SSH_PUB_KEY"
 sed -i "/\"keyData\":/c \"keyData\": \"${SSH_PUB_KEY}\"" oe-lnx.json
 ../bin/oe-engine generate --api-model oe-lnx.json
 
