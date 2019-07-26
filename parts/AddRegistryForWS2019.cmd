250c26dcb70b3ce1d8e38c39caca19f9ffe043ee
Shruti Ratnam <shratnam@microsoft.com>
2019-07-26 12:00:15 -0700

    Deleting AddRegistryForWS2019.cmd
    No more needed

--- parts/AddRegistryForWS2019.cmd
+++
@@ -1,6 +0,0 @@
-reg query HKLM\SYSTEM\CurrentControlSet\Services\sgx_lc_msr\Parameters /v SGX_Launch_Config_Optin
-if %ERRORLEVEL% EQU 0 goto SETUP
-goto:eof
-:SETUP
-reg add HKLM\SYSTEM\CurrentControlSet\Services\sgx_lc_msr\Parameters /v SGX_Launch_Config_Optin /t REG_DWORD /d 0x01 /s
-SHUTDOWN -r -t 10
